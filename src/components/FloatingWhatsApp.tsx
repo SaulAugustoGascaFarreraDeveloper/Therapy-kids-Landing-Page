7fed095abc450015944973b67c1120f7b3a8ca32
Saul Augusto Gasca Farrera <sgfarreraunity@gmail.com>
2026-03-27 11:38:25 -0600

    WhatsApp Predetermine message on teh href

--- src/components/FloatingWhatsApp.tsx
+++ src/components/FloatingWhatsApp.tsx
@@ -1,7 +1,7 @@
 export default function FloatingWhatsApp() {
   return (
     <a
-      href="https://wa.me/524428192020"
+      href="https://wa.me/524428192020?text=Hola%20👋🏼,%20quisiera%20conocer%20más%20sobre%20los%20servicios%20de%20Therapy%20Kids%20🌈"
       target="_blank"
       rel="noopener noreferrer"
       aria-label="Contactar por WhatsApp"
